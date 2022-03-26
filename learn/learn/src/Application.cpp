869ad1930ecf259c265ad50fdcc70fd3f572a0e7
DESKTOP-S844QQQ\liubo <liuboxing1995@live.cn>
2022-03-26 23:01:08 +0800

    VERTEX ATRR

--- learn/learn/src/Application.cpp
+++ learn/learn/src/Application.cpp
@@ -37,8 +37,9 @@ int main(void)
 	unsigned int buffer;
 	glGenBuffers(1, &buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-	glBufferData(GL_ARRAY_BUFFER, 6*sizeof(float), postions, GL_STATIC_DRAW);
-
+	glBufferData(GL_ARRAY_BUFFER, 6  * sizeof(float), postions, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float)*2, 0);
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
