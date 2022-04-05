b87c430392d3d66cae686d32ecce175eb747b76d
DESKTOP-S844QQQ\liubo <liuboxing1995@live.cn>
2022-04-05 21:50:39 +0800

    gl texture

--- learn/learn/src/Shader.h
+++ learn/learn/src/Shader.h
@@ -22,6 +22,8 @@ public:
 
 
 	// Set Unifroms
+	void SetUniform1i(const std::string& name, int value);
+	void setUniform1f(const std::string& name, float value);
 	void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
 private:
 	ShderPogramSource ParseShader(const std::string & filepath);
