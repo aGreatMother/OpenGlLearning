2d02cf2c86a21c7caef8a44d41c35670bf9dc4bb
DESKTOP-S844QQQ\liubo <liuboxing1995@live.cn>
2022-04-10 00:17:11 +0800

    math

--- learn/learn/src/Application.cpp
+++ learn/learn/src/Application.cpp
@@ -13,6 +13,9 @@
 #include "VertexArray.h"
 #include "Shader.h"
 #include "Texture.h"
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
 
 int main(void)
 {
@@ -69,9 +72,12 @@ int main(void)
 
 		IndexBuffer ib(indices, 6);
 
+		glm::mat4 proj = glm::ortho(-2.0f, 3.0f, -1.5f, 1.5f, -1.0f, 1.0f);
+
 		Shader shader("res/shaders/Basic.shader");
 		shader.Bind();
 		shader.SetUniform4f("u_Color",0.8f, 0.3f, 0.8f, 1.0f);
+		shader.setUniformMat4f("u_MVP", proj);
 		Texture texture("res/textures/image.png");
 		shader.SetUniform1i("u_Texture", 0);
 		texture.Bind();
--- learn/learn/src/Shader.h
+++ learn/learn/src/Shader.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include <unordered_map>
+#include "glm/glm.hpp"
 struct ShderPogramSource
 {
 	std::string VertexSource;
@@ -25,10 +26,12 @@ public:
 	void SetUniform1i(const std::string& name, int value);
 	void setUniform1f(const std::string& name, float value);
 	void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
+	void setUniformMat4f(const std::string& name, const glm::mat4& matrix);
 private:
 	ShderPogramSource ParseShader(const std::string & filepath);
 	unsigned int CompileShader(unsigned int type, const std::string & source);
 	unsigned int CreateShader(const std::string & vertexShader, const std::string & fragmentShader);
 	int GetUniformLocation(const std::string& name);
+
 };
 
